de73fc6e689870bdfdfc859d4206d3eb1b824cb8
Ramadhanmkoma <ramadhanmkoma9@gmail.com>
2024-02-02 17:09:51 +0300

    secure commit

--- src/app/telegram.config.ts
+++ src/app/telegram.config.ts
@@ -1,5 +1,5 @@
 export const telegramConfig = {
-  api_id: '23790439',
-  api_hash: '2081e44c8772b19d2817a73c55ee4ff0',
-  bot_token: '6656650705:AAE5jpDoRUmMZg6O5zxGUz0X2L1xvifVUdY'
+  api_id: '',
+  api_hash: '',
+  bot_token: ''
 }
